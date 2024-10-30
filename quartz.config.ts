5ed5ba2361a7075a1096014a3d4e63a26da332ad
wangzipai <wangzipaiya@gmail.com>
2024-10-30 15:02:23 +0800

    修改标题名称

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 cris' blog",
+    pageTitle: "🪴 Cris' Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,

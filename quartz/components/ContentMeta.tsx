581c0fcfddd5c3a401e5c72fb29fd37896340ecf
wangzipai <wangzipaiya@gmail.com>
2024-12-03 21:36:59 +0800

    解决编译问题

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -35,7 +35,7 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
             <span>
               <svg xmlns="http://www.w3.org/2000/svg" style="position:relative; top:2px;" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-plus"><path d="M8 2v4"/><path d="M16 2v4"/><path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8"/><path d="M3 10h18"/><path d="M16 19h6"/><path d="M19 16v6"/></svg> 创建于 {formatDate(fileData.dates.created,cfg.locale)}
             </span>
-            <div><div/>
+            <div></div>
           )
         }
   
@@ -44,7 +44,7 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
             <span>
               <svg xmlns="http://www.w3.org/2000/svg" style="position:relative; top:2px;" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-clock"><path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"/><path d="M16 2v4"/><path d="M8 2v4"/><path d="M3 10h5"/><path d="M17.5 17.5 16 16.3V14"/><circle cx="16" cy="16" r="6"/></svg> 更新于 {formatDate(fileData.dates.modified,cfg.locale)}
             </span>
-            <div><div/>
+            <div></div>
           )
         }
       }

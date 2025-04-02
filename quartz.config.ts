98c1c7e2da0ff4ef438864da710bcfd70c79af00
WANGYQ\wangyq <wangyq@ulandpower.com>
2025-04-02 15:37:45 +0800

    修改baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    // baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "020514.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
